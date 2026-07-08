7d8ba72db3a4ae0fb5a89beb45a45eafa6f702b7
jadriendev <ddarkknife@gmail.com>
2026-07-08 18:20:03 +0800

    feat: navbar and footer

---
+++ script.js
@@ -0,0 +1,22 @@
+const menuBtn = document.getElementById("menuBtn");
+const closeBtn = document.getElementById("closeBtn");
+const sidebar = document.getElementById("sidebar");
+const overlay = document.getElementById("overlay");
+
+function openMenu() {
+    sidebar.classList.remove("-translate-x-full");
+
+    overlay.classList.remove("opacity-0", "invisible");
+}
+
+function closeMenu() {
+    sidebar.classList.add("-translate-x-full");
+
+    overlay.classList.add("opacity-0", "invisible");
+}
+
+menuBtn.addEventListener("click", openMenu);
+
+closeBtn.addEventListener("click", closeMenu);
+
+overlay.addEventListener("click", closeMenu);
